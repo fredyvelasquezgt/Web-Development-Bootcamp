b763e315f884210bfbfd168157fc99483be32e38
Fredy Velásquez <macacoloco696@gmail.com>
2024-02-25 21:30:35 -0600

    logic added

--- 14 JSB/file.js
+++ 14 JSB/file.js
@@ -75,5 +75,22 @@ function movement() {
 }
 
 function getMilk () {
-  
+  console.log("leaveHouse");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("enterHouse");
 }
