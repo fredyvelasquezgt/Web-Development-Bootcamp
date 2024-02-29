c7eec2ee3bdf938078af718491603570ce20f13f
Fredy Velásquez <macacoloco696@gmail.com>
2024-02-28 23:13:51 -0600

    changes

--- 14 JSB/file.js
+++ 14 JSB/file.js
@@ -144,3 +144,14 @@ var name2 = prompt('Ingresa el otro nombre: ')
 n = Math.random();
 n = n * 100 + 1;
 console.log('Tu probabilidad de amor es: ' +  Math.floor(n) + '%');
+
+function bmiCalculator(weight, height) {
+  var bmi = weight / (height * height);
+  if (bmi < 18.5) {
+      return "Your BMI is " + bmi + ", so you are underweight.";
+  } else if (bmi >= 18.5 && bmi <= 24.9) {
+      return "Your BMI is " + bmi + ", so you have a normal weight.";
+  } else {
+      return "Your BMI is " + bmi + ", so you are overweight.";
+  }
+}
