6563c08dee6d8e35687ccee8611fd20923beb6d2
Fredy Velásquez <macacoloco696@gmail.com>
2024-03-02 19:42:37 -0600

    js

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,6 +1,7 @@
 
-document.querySelector("button").addEventListener("click", handleClick);
-
-function handleClick() {
-    alert('i got clicked!')
+for(var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+        alert('i got clicked!')
+    });
 }
+
