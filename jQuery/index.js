230afe49896107ff8852b374ee95a884176171dd
Fredy Velásquez <macacoloco696@gmail.com>
2024-03-03 21:30:13 -0600

    jQuery

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,6 +1,26 @@
 
-$("h1").addClass("big-title margin-50");
+//$("h1").text("Bye")
 
-$("button");
+//$("button").html("<em>pls click me</em>")
 
-$("h1").hasClass("margin-50")
+//$("h1").hasClass("margin-50")
+
+//attributes
+
+//$("img").attr("src")
+
+//$("a").attr("href", "htt")
+
+//eventListeners
+
+// $("h1").click(function() {
+//     $("h1").css("color", "purple")
+// })
+
+// $(document).keypress(function(event){
+//     $("h1").text(event.key);
+// })
+
+$(document).on("mouseover", function() {
+    $("h1").css("color", "purple")
+})
