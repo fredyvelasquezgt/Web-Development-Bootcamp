d48beb2b54cd342d8dfc723e252920b7dec32419
Fredy Velásquez <macacoloco696@gmail.com>
2024-03-10 15:32:59 -0600

    express

--- Backend/3.1 Express Server/index.js
+++ Backend/3.1 Express Server/index.js
@@ -1,1 +1,8 @@
 //Creating my first server
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
