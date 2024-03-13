ff8b9552bece45a802917e8ce69f7e3ddb272910
Fredy Velásquez <macacoloco696@gmail.com>
2024-03-13 12:58:56 -0600

    middleware

--- Backend/3.4 Middleware/index1.js
+++ Backend/3.4 Middleware/index1.js
@@ -1,15 +1,14 @@
 import express from "express";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-const __dirname = dirname(fileURLToPath(import.meta.url));
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
-//sending res
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
-});
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.post("/submit", (req, res) => {
+  console.log(req.body)
+})
 
 //listening to port
 app.listen(port, () => {
