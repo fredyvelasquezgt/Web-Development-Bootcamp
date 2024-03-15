b5de0201db1b644807c933ae76b4e9c574ca444d
Fredy Velásquez <macacoloco696@gmail.com>
2024-03-15 11:01:06 -0600

    react

--- React/280-jsx-code-challenge/src/index.jsx
+++ React/280-jsx-code-challenge/src/index.jsx
@@ -8,3 +8,23 @@
 // to install the node modules and
 // npm run dev
 // to launch your react project in your browser
+
+//Create a react app from scratch.
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    {" "}
+    <h1> My Favorite Foods</h1>{" "}
+    <ul>
+      {" "}
+      <li>Food 1</li> <li>Food 2</li> <li>Food 3</li>{" "}
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
