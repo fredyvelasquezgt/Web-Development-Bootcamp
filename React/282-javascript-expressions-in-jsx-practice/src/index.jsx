ef50348d4fa3afea5a3436aa4019a4cad1789f24
Fredy Velásquez <macacoloco696@gmail.com>
2024-03-16 11:22:50 -0600

    react

--- React/282-javascript-expressions-in-jsx-practice/src/index.jsx
+++ React/282-javascript-expressions-in-jsx-practice/src/index.jsx
@@ -12,3 +12,25 @@
 // to install the node modules and
 // npm run dev
 // to launch your react project in your browser
+
+//Create a react app from scratch.
+//It should display 2 paragraph HTML elements.
+//The paragraphs should say:
+//Created by YOURNAME.
+//Copyright CURRENTYEAR.
+//E.g.
+//Created by Angela Yu.
+//Copyright 2019.
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const name = "Fredy";
+
+ReactDOM.render(
+  <div>
+    {" "}
+    <p> Created by {name} </p> <p> Copyright {new Date().getFullYear()} </p>{" "}
+  </div>,
+  document.getElementById("root")
+);
