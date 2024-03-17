b068882e1a989ef31f49157a1fc4cb6cfcb47085
Fredy Velásquez <macacoloco696@gmail.com>
2024-03-17 12:59:36 -0600

    react

--- React/286-react-components/src/index.jsx
+++ React/286-react-components/src/index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Heading from "./Heading";
 
 ReactDOM.render(
   <div>
-    <h1>My Favourite Foods</h1>
+    <Heading />
+    <h2>My Favourite Foods</h2>
     <ul>
       <li>Bacon</li>
       <li>Jamon</li>
@@ -12,9 +14,3 @@ ReactDOM.render(
   </div>,
   document.getElementById("root")
 );
-
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
