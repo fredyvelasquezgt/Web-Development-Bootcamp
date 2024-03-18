f3da9ead2193d66b8fa8f1aaaeb7242657af290e
Fredy Velásquez <macacoloco696@gmail.com>
2024-03-18 10:25:28 -0600

    react

--- React/293-keeper-app-part-1-challenge/src/index.jsx
+++ React/293-keeper-app-part-1-challenge/src/index.jsx
@@ -16,3 +16,14 @@
 // to install the node modules and
 // npm run dev
 // to launch your react project in your browser
+
+//6. Make sure that the final website is styled like the example shown here:
+//https://l1pp6.csb.app/
+
+//HINT: You will need to study the classes in teh styles.css file to appy styling.
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
