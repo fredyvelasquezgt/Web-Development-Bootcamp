368992d88f24614074c9a1cf8c86f9d6bfc2bdcf
Fredy Velásquez <macacoloco696@gmail.com>
2024-03-18 11:31:51 -0600

    react

--- React/295-react-props/src/index.jsx
+++ React/295-react-props/src/index.jsx
@@ -1,33 +1,52 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import contacts from "../contacts.js";
+
+//1. Apply CSS styles to App.jsx component
+//to match the appearance on the completed app:
+//https://c6fkx.csb.app/
 
 function Contact(props) {
   return (
     <div>
-      <h2>{props.name}</h2>
-      <img src={props.src} alt="avatar_img" />
-      <p>{props.tel}</p>
-      <p>{props.email}</p>
+      <div className="card">
+        <div className="top">
+          <h2 className="name">{props.name}</h2>
+          <img className="circle-img" src={props.src} alt="avatar_img" />
+        </div>
+        <div className="bottom">
+          <p className="info">{props.tel}</p>
+          <p className="info">{props.email}</p>
+        </div>
+      </div>
     </div>
   );
 }
 
-ReactDOM.render(
-  <div>
-    <h1>My Contacts</h1>
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
 
-    <Contact
-      name="Beyonce"
-      src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-      tel="+123 456 789"
-      email="b@beyonce.com"
-    />
-    <Contact
-      name="Fredy Velasquez"
-      src="https://i.imgur.com/yXOvdOSs.jpg"
-      tel="12345"
-      email="fredy@gmail.com"
-    />
-  </div>,
-  document.getElementById("root")
-);
+      <Contact
+        name={contacts[0].name}
+        src={contacts[0].imgURL}
+        tel={contacts[0].phone}
+        email={contacts[0].email}
+      />
+      <Contact
+        name={contacts[1].name}
+        src={contacts[1].imgURL}
+        tel={contacts[1].phone}
+        email={contacts[1].email}
+      />
+      <Contact
+        name={contacts[2].name}
+        src={contacts[2].imgURL}
+        tel={contacts[2].phone}
+        email={contacts[2].email}
+      />
+    </div>
+  );
+}
+
+export default App;
