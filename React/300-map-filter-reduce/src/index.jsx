b4e850d682af33e03ac08766d498c009047e0514
Fredy Velásquez <macacoloco696@gmail.com>
2024-03-19 13:10:13 -0600

    react

--- React/300-map-filter-reduce/src/index.jsx
+++ React/300-map-filter-reduce/src/index.jsx
@@ -1,15 +1,19 @@
 var numbers = [3, 56, 2, 48, 5];
 
 //Map -Create a new array by doing something with each item in an array.
+//Map is more consice than forEach
+// function double(x) {
+//   return x * 2;
+// }
 
-function double(x) {
-  return x * 2;
-}
-
-const newNumber = numbers.map(double);
-console.log(newNumber);
+// const newNumber = numbers.map(double);
+// console.log(newNumber);
 
 //Filter - Create a new array by keeping the items that return true.
+
+numbers.filter(function (num) {
+  return num > 10;
+});
 
 //Reduce - Accumulate a value by doing something to each item in an array.
 
