1dba35912bb5b49de27136c953cb22dc5f3a12bf
Fredy Velásquez <macacoloco696@gmail.com>
2024-03-26 11:09:53 -0600

    react

---
+++ React/308-es6-destructuring/src/index.js
@@ -0,0 +1,28 @@
+import animals from "./data";
+
+const [cat, dog] = animals;
+
+// CHALLENGE: uncomment the code below and see the car stats rendered
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import cars from "./practice";
+
+// ReactDOM.render(
+//   <table>
+//     <tr>
+//       <th>Brand</th>
+//       <th>Top Speed</th>
+//     </tr>
+//     <tr>
+//       <td>{tesla.model}</td>
+//       <td>{teslaTopSpeed}</td>
+//       <td>{teslaTopColour}</td>
+//     </tr>
+//     <tr>
+//       <td>{honda.model}</td>
+//       <td>{hondaTopSpeed}</td>
+//       <td>{hondaTopColour}</td>
+//     </tr>
+//   </table>,
+//   document.getElementById("root")
+// );
