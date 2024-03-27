c6e049d9ed5c1a225a4112bf73b832d83c202846
Fredy Velásquez <macacoloco696@gmail.com>
2024-03-26 22:18:33 -0600

    react

--- React/310-event-handling-in-react/src/components/App.jsx
+++ React/310-event-handling-in-react/src/components/App.jsx
@@ -3,15 +3,32 @@ import React, { useState } from "react";
 function App() {
   const [headingText, setHeadingText] = React.useState("Hello");
 
+  const [colorButton, setColorButton] = React.useState("white");
+
   function handleClick() {
     setHeadingText("submitted");
+  }
+
+  function blackButton() {
+    setColorButton("black");
+  }
+
+  function whiteButton() {
+    setColorButton("white");
   }
 
   return (
     <div className="container">
       <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button onClick={handleClick}>Submit</button>
+      <button
+        style={{ backgroundColor: colorButton }}
+        onClick={handleClick}
+        onMouseOver={blackButton}
+        onMouseOut={whiteButton}
+      >
+        Submit
+      </button>
     </div>
   );
 }
