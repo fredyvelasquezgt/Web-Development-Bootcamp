865a6aa4cf6eac7729ef4c5f6863044f961c231c
Fredy Velásquez <macacoloco696@gmail.com>
2024-04-01 12:17:20 -0600

    react

--- React/316-es6-spread-operator-practice/src/components/App.jsx
+++ React/316-es6-spread-operator-practice/src/components/App.jsx
@@ -1,20 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+
+//CHALLENGE: Make this app work by applying what you've learnt.
+//2. When the add button is pressed, the current data in the input should be
+//added to an array.
+//3. The <ul> should display all the array items as <li>s
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
+
+  function addItem() {
+    setItems((prevItems) => {
+      return [...prevItems, inputText];
+    });
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input type="text" value={inputText} onChange={handleChange} />
+        <button onClicl={addItem}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item</li>
+          {items.map((item) => {
+            return <li>{item}</li>;
+          })}
         </ul>
       </div>
     </div>
