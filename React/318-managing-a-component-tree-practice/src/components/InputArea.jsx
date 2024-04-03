bd1db86c503bfd1d65978dbdce204307a8575ecc
Fredy Velásquez <macacoloco696@gmail.com>
2024-04-03 11:00:19 -0600

    react

--- React/318-managing-a-component-tree-practice/src/components/InputArea.jsx
+++ React/318-managing-a-component-tree-practice/src/components/InputArea.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 function InputArea() {
+  const [inputText, setInputText] = useState("");
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
+
   return (
     <div className="form">
       <input onChange={handleChange} type="text" value={inputText} />
