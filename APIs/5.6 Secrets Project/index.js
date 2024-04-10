d4074059b71fdab4504e25f62838fa500e0cc377
Fredy Velásquez <macacoloco696@gmail.com>
2024-04-09 23:37:26 -0600

    apis

--- APIs/5.6 Secrets Project/index.js
+++ APIs/5.6 Secrets Project/index.js
@@ -12,8 +12,8 @@ app.get("/", async (req, res) => {
         const result = await axios.get(API_URL+"/random");
         res.render("index.ejs", {secret: JSON.stringify(result.data.secret), user: JSON.stringify(result.data.username)})
     } catch (error) {
-        res.render("index.ejs", )
-
+        console.log(error.response.data)
+        res.status(500);
     }
 })
 
