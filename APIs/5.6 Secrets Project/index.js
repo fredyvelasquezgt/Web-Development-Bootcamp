3cdd8bab96e5ea7f75a9198967a9009fbdf1e6bb
Fredy Velásquez <macacoloco696@gmail.com>
2024-04-09 23:36:09 -0600

    apis

--- APIs/5.6 Secrets Project/index.js
+++ APIs/5.6 Secrets Project/index.js
@@ -5,15 +5,16 @@ const app = express()
 const port = 3000;
 const API_URL = "https://secrets-api.appbrewery.com";
 
+app.user(express.static("public"));
 
 app.get("/", async (req, res) => {
     try {
         const result = await axios.get(API_URL+"/random");
         res.render("index.ejs", {secret: JSON.stringify(result.data.secret), user: JSON.stringify(result.data.username)})
     } catch (error) {
-        
+        res.render("index.ejs", )
+
     }
-    res.render("index.ejs", )
 })
 
 
