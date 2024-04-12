8bb4c430fdfae1e6c34dce15043e30e0e0b4e3ed
Fredy Velásquez <macacoloco696@gmail.com>
2024-04-11 23:19:30 -0600

    GET api

--- MyOwnAPI/6.0 DIY API/index.js
+++ MyOwnAPI/6.0 DIY API/index.js
@@ -9,6 +9,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //1. GET a random joke
 
+app.get("/random", (req, res) => {
+    const randomIndex = Math.floor(Math.random()*jokes.length)
+    res.json(jokes[randomIndex]);
+})
+
 //2. GET a specific joke
 
 //3. GET a jokes by filtering on the joke type
