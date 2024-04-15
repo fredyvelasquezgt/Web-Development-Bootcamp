689a787290900cc9dddb40e74a7223d32d82a11c
Fredy Velásquez <macacoloco696@gmail.com>
2024-04-15 14:57:01 -0600

    api

--- APIs/6.1+Blog+API+Project/index.js
+++ APIs/6.1+Blog+API+Project/index.js
@@ -76,6 +76,13 @@ app.post("/posts",(req, res) => {
 
 //CHALLENGE 4: PATCH a post when you just want to update one parameter
 
+app.patch("/posts/:id", (req, res) => {
+  const id = parseInt(req.params.id)
+  const post = posts.find((p) => p.id === id);
+  if (!post) return res.status(404).json({message: "Post not found" })
+})
+
+
 //CHALLENGE 5: DELETE a specific post by providing the post id.
 
 app.listen(port, () => {
