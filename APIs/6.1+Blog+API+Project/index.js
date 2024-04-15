2af2e0ace975f2cacbde0235bbd3a7cb36537c73
Fredy Velásquez <macacoloco696@gmail.com>
2024-04-15 14:59:20 -0600

    api

--- APIs/6.1+Blog+API+Project/index.js
+++ APIs/6.1+Blog+API+Project/index.js
@@ -80,6 +80,9 @@ app.patch("/posts/:id", (req, res) => {
   const id = parseInt(req.params.id)
   const post = posts.find((p) => p.id === id);
   if (!post) return res.status(404).json({message: "Post not found" })
+  
+  if(req.body.title) post.title = req.body.title;
+
 })
 
 
