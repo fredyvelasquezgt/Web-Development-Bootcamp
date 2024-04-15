ca7385f9baa3f83132e532fc1c98b97ceeddf51e
Fredy Velásquez <macacoloco696@gmail.com>
2024-04-15 13:13:45 -0600

    api

--- MyOwnAPI/6.0 DIY API/index.js
+++ MyOwnAPI/6.0 DIY API/index.js
@@ -80,9 +80,26 @@ app.patch("jokes/:id", (req, res) => {
 
 //7. DELETE Specific joke
 
+app.delete("jokes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const searchIndex = jokes.findIndex((jokes) => joke.id === id)
+  if(searchIndex > -1) {
+    jokes.splice(searchIndex, 1);
+    res.sendStatus(200)
+  } else {
+    res
+      .status(404)
+      .json({error: `Joke with  the id: ${id} not found. No jokes detected`})
+  }
+
+})
 
 
 //8. DELETE All jokes
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Successfully started server on port ${port}.`);
