c9d9d4832829538c234f6f770618758c9bee545b
Fredy Velásquez <macacoloco696@gmail.com>
2024-04-15 13:19:27 -0600

    api

--- MyOwnAPI/6.0 DIY API/index.js
+++ MyOwnAPI/6.0 DIY API/index.js
@@ -84,20 +84,29 @@ app.delete("jokes/:id", (req, res) => {
   const id = parseInt(req.params.id);
   const searchIndex = jokes.findIndex((jokes) => joke.id === id)
   if(searchIndex > -1) {
-    jokes.splice(searchIndex, 1);
+    jokes.splice(searchIndex, 1); //removes one element from the jokes array based on the searchIndex 
     res.sendStatus(200)
   } else {
     res
       .status(404)
       .json({error: `Joke with  the id: ${id} not found. No jokes detected`})
   }
-
 })
 
 
 //8. DELETE All jokes
 
-
+app.delete("all", (req, res) => {
+  const userKey = req.query.key;
+  if(userKey = masterKey) {
+    jokes = [];
+    res.sendStatus(200)
+  } else {
+    res
+      .status(404)
+      .json({error: `You are not authorized to perform this action`})
+  }
+})
 
 
 
