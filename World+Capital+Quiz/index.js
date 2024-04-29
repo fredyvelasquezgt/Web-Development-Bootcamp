7ad2f121862342a2c118374a63b2764f1c9af275
Fredy Velásquez <macacoloco696@gmail.com>
2024-04-29 12:35:56 -0600

    db

--- World+Capital+Quiz/index.js
+++ World+Capital+Quiz/index.js
@@ -1,8 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "fredestroyer",
+  port: 5432
+})
+
 
 let quiz = [
   { country: "France", capital: "Paris" },
